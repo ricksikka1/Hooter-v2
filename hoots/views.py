ca5e4efc7a1e19db2ca05254dce9fb97cfaf49c1
Raghav Sikka <ricksikka1@gmail.com>
2021-05-06 20:24:55 -0400

    dynamic url routing

--- hoots/views.py
+++ hoots/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Hoot
 
@@ -9,10 +9,18 @@ def home_view(request, *args, **kwargs):
 
 def hoot_detail_view(request, hoot_id, *args, **kwargs):
     """
-    
+    Return REST API View (note 5)
     """
+    data = {
+        "id": hoot_id,
+    }
+    status = 200
+
     try:
         obj = Hoot.objects.get(id=hoot_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {hoot_id} - {obj.content}")
+        status = 404
+        data['message'] = "Not Found"
+
+    return JsonResponse(data, status=status)
