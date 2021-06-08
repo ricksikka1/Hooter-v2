b95c2ec8d5cc7cc50ed15604cd928a5f6c76b2f6
Raghav Sikka <ricksikka1@gmail.com>
2021-06-07 23:45:31 -0400

    profiles following api unit tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,15 +1,23 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from rest_framework.test import APIClient
+
+from .models import Profile
+
 
 User = get_user_model()
-# Create your tests here.
-from .models import Profile
 
 class ProfileTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='rick', password='somepassword')
         self.userb = User.objects.create_user(username='rick-2', password='somepassword2')
     
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepassword')
+        return client
+
     def test_profile_created_via_signal(self):
         qs = Profile.objects.all()
         self.assertEqual(qs.count(), 2)
@@ -23,3 +31,26 @@ class ProfileTestCase(TestCase):
         first_user_following_no_one = first.following.all() # check new user has is not following anyone
         self.assertTrue(qs.exists())
         self.assertFalse(first_user_following_no_one.exists())
+
+    def test_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow",
+            {"action": "follow"}
+        )
+        r_data = response.json()
+        count = r_data.get("followers")
+        self.assertEqual(count, 1)
+    
+    def test_unfollow_api_endpoint(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second)
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow",
+            {"action": "unfollow"}
+        )
+        r_data = response.json()
+        count = r_data.get("followers")
+        self.assertEqual(count, 0)
