7d33bfa7b5f2f28d11d0eed434c3e5cec81d621a
Raghav Sikka <ricksikka1@gmail.com>
2021-06-11 14:44:14 -0400

    redundant follow view

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import (
-    user_follow_view,
     profile_detail_api_view
 )
 '''
 BASE Endpoint /api/profiles/
 '''
 urlpatterns = [
-    path('<str:username>/follow', user_follow_view),
+    path('<str:username>/follow', profile_detail_api_view),
     path('<str:username>/', profile_detail_api_view)
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -12,42 +12,22 @@ from ..serializers import PublicProfileSerializer
 
 User = get_user_model()
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def profile_detail_api_view(request, username, *args, **kwargs):
-    # Get the profile for the username
+    me = request.user
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
         return Response({"detail": "User not found"}, status=404)
     profile_obj = qs.first()
-    data = PublicProfileSerializer(instance=profile_obj, context={"request": request})
-    print(request.user.id)
-    return Response(data.data, status=200)
-
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def user_follow_view(request, username, *args, **kwargs):
-    me = request.user
-    other_user_qs = User.objects.filter(username=username)
-    if not other_user_qs.exists():
-        return Response({}, status=404)
-    other = other_user_qs.first()
-    profile = other.profile
-
     data = request.data or {}
-    action = data.get("action")
-    if action == "follow":
-        profile.followers.add(me)
-    elif action == "unfollow":
-        profile.followers.remove(me)
-    else:
-        pass
-
-    data = PublicProfileSerializer(instance=profile, context={"request": request})
-    return Response(data.data, status=200)
-
-# @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def user_profile_detail_view(request, username, *args, **kwargs):
-#     current_user = request.user
-#     to_follow_user = 
-#     return Response({}, status=200)
+    if request.method == "POST":
+        action = data.get("action")
+        if profile_obj.user != me:
+            if action == "follow":
+                profile_obj.followers.add(me)
+            elif action == "unfollow":
+                profile_obj.followers.remove(me)
+            else:
+                pass
+    serializer = PublicProfileSerializer(instance=profile_obj, context={"request": request})
+    return Response(serializer.data, status=200)
